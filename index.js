91ae285dbb1445284bc1e510925576c949e005fc
Adeline Almanzar <almanzar.adeline@gmail.com>
2021-12-19 15:32:30 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Write your solution in this file!
+const employee = {
+    name: "karen",
+    streetAddress: "broadway", //might need to add nested address
+};
+employee;
+function updateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    const newEmployee = {
+        ...employeeObj,
+        [key]: value,
+    };
+    return newEmployee;
+};
+updateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "Sam"
+);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employeeObj, key, value) {
+    employeeObj[key] = value;
+    return employee
+};
+destructivelyUpdateEmployeeWithKeyAndValue(
+    employee,
+    "name",
+    "Sam",
+);
+destructivelyUpdateEmployeeWithKeyAndValue(
+    employee,
+    "address",
+    "11 Broadway",
+);
+
+for (const key in employee) {
+    delete employee[key];
+};
+employee.name = "Sam";
+function deleteFromEmployeeByKey(employeeObj, key) {
+    const copyEmployee = {
+        ...employeeObj,
+    };
+    delete copyEmployee[key];
+    return copyEmployee
+};
+deleteFromEmployeeByKey(
+    employee,
+    "name",
+);
+
+function destructivelyDeleteFromEmployeeByKey(employeeObj, key) {
+    delete employeeObj[key];
+    return employee
+};
+destructivelyDeleteFromEmployeeByKey(
+    employee,
+    "name",
+);
+
